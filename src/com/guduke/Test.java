80d41beeb61b2c017415043e8b2310c256862c49
guduke <1287286145@qq.com>
2018-10-17 20:23:24 +0800

    Update Test.java
    213123

--- src/com/guduke/Test.java
+++ src/com/guduke/Test.java
@@ -18,5 +18,6 @@ public class Test {
         NowApiWeather nowAipWeather = WeatherUtil.getNowAipWeather("2", "37304", "c0c89fd4c13dfe5816186052fd0a185c");
         System.out.println(nowAipWeather);
         System.out.println("Test.main");
+        //测试
     }
 }
